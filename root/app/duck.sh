f3803bed9527eeb9a3ebc53a6b6fddbd2c7ec4a4
weberpilipp <philipp.weber@zohomail.eu>
2022-07-12 20:12:51 +0200

    Added IPv6 and logging for IPv4 and IPv6

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/with-contenv bash
 
 . /app/duck.conf
-RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
+IPV4=$(curl -4 -sS --max-time 60 ip.me)
+IPV6=$(curl -6 -sS --max-time 60 ip.me)
+RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}&ipv6=${IPV6}")
 if [ "${RESPONSE}" = "OK" ]; then
     echo "Your IP was updated at $(date)"
+    echo "IPv4: ${IPV4}"
+    echo "IPv6: ${IPV6}"
 else
     echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
 fi
