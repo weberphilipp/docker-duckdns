cdbb247bd187e2384fa271a2586fc807dc669af0
weberpilipp <philipp.weber@zohomail.eu>
2022-07-12 20:51:46 +0200

    Added ability to enable/disable IPv6 by env var

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -2,12 +2,18 @@
 
 . /app/duck.conf
 IPV4=$(curl -4 -sS --max-time 60 ip.me)
-IPV6=$(curl -6 -sS --max-time 60 ip.me)
-RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}&ipv6=${IPV6}")
+if [ "$IPV6" = "true" ]; then
+    IPV6=$(curl -6 -sS --max-time 60 ip.me)
+    RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}&ipv6=${IPV6}")
+else
+    RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}")
+fi
 if [ "${RESPONSE}" = "OK" ]; then
     echo "Your IP was updated at $(date)"
     echo "IPv4: ${IPV4}"
-    echo "IPv6: ${IPV6}"
+    if [ "$IPV6" = "true" ]; then
+        echo "IPv6: ${IPV6}"
+    fi
 else
     echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
 fi
