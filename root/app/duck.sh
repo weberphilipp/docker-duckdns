d453b40be7e93cfaa5fd69f002d542853dccc9ac
weberpilipp <philipp.weber@zohomail.eu>
2022-07-12 21:38:27 +0200

    Added ability to disable IPv4 updates

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/with-contenv bash
 
 . /app/duck.conf
-IPV4=$(curl -4 -sS --max-time 60 ip.me)
+URL="https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}"
+if [ "$IPV4" != "false" ]; then
+    IPV4=$(curl -4 -sS --max-time 60 ip.me)
+    URL="${URL}&ip=${IPV4}"
+    echo "IPv4: ${IPV4}"
+fi
 if [ "$IPV6" != "false" ]; then
     IPV6=$(curl -6 -sS --max-time 60 ip.me)
-    RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}&ipv6=${IPV6}")
-else
-    RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}")
+    URL="${URL}&ipv6=${IPV6}"
+    echo "IPv6: ${IPV6}"
 fi
+RESPONSE=$(curl -sS --max-time 60 "${URL}")
 if [ "${RESPONSE}" = "OK" ]; then
     echo "Your IP was updated at $(date)"
-    echo "IPv4: ${IPV4}"
-    if [ "$IPV6" != "false" ]; then
-        echo "IPv6: ${IPV6}"
-    fi
 else
     echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
 fi
